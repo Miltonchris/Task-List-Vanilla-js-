66a718fb1b1c4af9fee53b34f4a8d7517bdafc52
milton <christodouloumilton@gmail.com>
2018-02-23 15:17:09 +0000

    update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Vars 
+// Selectors 
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearButton = document.querySelector('.clear-tasks');
